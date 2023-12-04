262153b76980b14adf4e01533a2a300005e0b92e
Magamedrasul Ibragimov <curryrasul@gmail.com>
2023-12-04 10:51:46 +0400

    chore: implement GenerateAst

--- jlox/src/GenerateAst.java
+++ jlox/src/GenerateAst.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GenerateAst {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.err.println("Usage: generate_ast <output directory>");
             System.exit(64);
@@ -30,9 +30,31 @@ public class GenerateAst {
         for (String type: types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
         }
 
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, 
+            String className, String fieldList) {
+        writer.println("    static class " + className + " extends " + baseName + " {");
+
+        writer.println("        " + className + "(" + fieldList + ") {");
+        String[] fields = fieldList.split(", ");
+        for (String field: fields) {
+            String name = field.split(" ")[1];
+            writer.println("            this." + name + " = " + name + ";");
+        }   
+        writer.println("        }");
+
+        writer.println();
+
+        for (String field: fields) {
+            writer.println("        final " + field + ";");
+        }
+        writer.println("    }");
+        writer.println();
     }
 }
