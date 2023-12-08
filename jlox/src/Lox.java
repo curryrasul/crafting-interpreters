550670569ef73e5874933bc88b2cf66d3d9b0622
Magamedrasul Ibragimov <curryrasul@gmail.com>
2023-12-08 13:24:10 +0400

    chore: implement simple evaluator

--- jlox/src/Lox.java
+++ jlox/src/Lox.java
@@ -7,7 +7,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -25,6 +27,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
         
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -48,7 +51,7 @@ public class Lox {
 
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -66,5 +69,10 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
